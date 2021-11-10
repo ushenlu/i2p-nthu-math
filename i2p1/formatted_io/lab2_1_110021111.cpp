626a4dd11d16cf465deca0d4a19ebe292b30f719
stevenokm <stevenokm@users.noreply.github.com>
2021-11-10 16:22:45 +0000

    deploy: 713bcc1e1886e505fb73a4158851fddc786c4ec8

---
+++ i2p1/formatted_io/lab2_1_110021111.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b;
+    cout << "Input real number, before decimal point: ";
+    cin >> a;
+    cout << "Input real number, after decimal point: ";
+    cin >> b;
+    cout << "The real number is: " << a + b / 1000000 << endl;
+    return 0;
+}
